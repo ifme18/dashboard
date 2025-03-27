45f216af03ba8c18fef0472145158d4a6651ee8b
ken sofware engineer <mainakenken188@gmail.com>
2025-03-27 06:48:57 +0300

    changes made "few"

---
+++ hardhat.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    solidity: "0.8.0",
+    networks: {
+      ganache: {
+        url: "http://127.0.0.1:7545",
+        accounts: ["your-private-key-from-ganache"]
+      }
+    }
+  };
